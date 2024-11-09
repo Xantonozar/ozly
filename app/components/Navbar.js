37d68f746075e3d2ea5ec1b7a809afa508a3a93a
Ozar <87942063+Xantonozar@users.noreply.github.com>
2024-11-09 08:47:06 +0600

    Add files via upload

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -16,19 +16,19 @@ const Navbar = () => {
   <div className="flex flex-row justify-between  md:flex-row items-center">
     <ul className="hidden md:flex md:flex-row md:space-x-8 space-y-4 md:space-y-0">
       <li className="text-green-200 hover:text-green-900 transition duration-300 ease-in-out transform hover:scale-110 hover:text-lg text-lg font-semibold">
-        <a href="/">Home</a>
+        <Link href="/">Home</Link>
       </li>
       <li className="text-green-200 hover:text-green-900 transition duration-300 ease-in-out transform hover:scale-110 hover:text-lg text-lg font-semibold">
-        <a href="/about">About</a>
+        <Link href="/about">About</Link>
       </li>
       <li className="text-green-200 hover:text-green-900 transition duration-300 ease-in-out transform hover:scale-110 hover:text-lg text-lg font-semibold">
-        <a href="/contact">Contact Us</a>
+        <Link href="/contact">Contact Us</Link>
       </li>
       <li className="text-green-200 hover:text-green-900 transition duration-300 ease-in-out transform hover:scale-110 hover:text-lg text-lg font-semibold">
-        <a href="/shorten">Shorten</a>
+        <Link href="/shorten">Shorten</Link>
       </li>
       <li className="text-green-200 hover:text-green-900 transition duration-300 ease-in-out transform hover:scale-110 hover:text-lg text-lg font-semibold">
-        <a href="/url">URL</a>
+        <Link href="/url">URL</Link>
       </li>
       <li className="text-green-200 hover:text-green-900 transition duration-300 ease-in-out transform hover:scale-110 hover:text-lg text-lg font-semibold">
        
@@ -36,10 +36,10 @@ const Navbar = () => {
     </ul>
     <div className="hidden flex-row justify-end md:flex space-x-4 mt-4 md:mt-0">
       <button className="bg-green-400 text-black font-bold px-4 py-2 rounded hover:bg-green-700 hover:text-white transition duration-300 ease-in-out transform hover:scale-105">
-        <a href="/shorten">Try Now</a>
+        <Link href="/shorten">Try Now</Link>
       </button>
       <button className="bg-green-600 text-black font-bold px-4 py-2 rounded hover:bg-green-700 hover:text-white transition duration-300 ease-in-out transform hover:scale-105">
-        <a href="https://github.com/Xantonozar/ozly.git">GitHub</a>
+        <Link href="https://github.com/Xantonozar/ozly.git">GitHub</Link>
       </button>
     </div>
 
@@ -51,18 +51,18 @@ const Navbar = () => {
 </div>
 <div className={`sidebar md:hidden transform transition-transform duration-300 ${isOpen ? 'translate-x-0 flex' : 'translate-x-full hidden'}  flex-col`} style={{ height: '100%', width: '50.66%' }}>
   <nav className="flex flex-col mt-10">
-    <a href="/home" className="py-2 px-4 hover:bg-green-900">Home</a>
-    <a href="/about" className="py-2 px-4 hover:bg-green-900">About</a>
-    <a href="/shorten" className="py-2 px-4 hover:bg-green-900">Services</a>
-    <a href="/contact" className="py-2 px-4 hover:bg-green-900">Contact</a>
+    <Link href="/home" className="py-2 px-4 hover:bg-green-900">Home</Link>
+    <Link href="/about" className="py-2 px-4 hover:bg-green-900">About</Link>
+    <Link href="/shorten" className="py-2 px-4 hover:bg-green-900">Services</Link>
+    <Link href="/contact" className="py-2 px-4 hover:bg-green-900">Contact</Link>
 
   </nav>
   <div className="flex flex-row mb-4 justify-start gap-4 md:hidden space-x-4 mt-4 md:mt-0">
       <button  className="bg-green-400 text-black font-bold px-4 py-2 rounded hover:bg-green-700 hover:text-white transition duration-300 ease-in-out transform hover:scale-105">
-        <a href="/shorten">Try Now</a>
+        <Link href="/shorten">Try Now</Link>
       </button>
       <button className="bg-green-600 text-black font-bold px-4 py-2 rounded hover:bg-green-700 hover:text-white transition duration-300 ease-in-out transform hover:scale-105">
-      <a href="https://github.com/Xantonozar/ozly.git">GitHub</a>
+      <Link href="https://github.com/Xantonozar/ozly.git">GitHub</Link>
       </button>
     </div>
 </div>
