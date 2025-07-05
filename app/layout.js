1f54713078b7acc5660d64c5d16a5e352f050b23
Ozar <87942063+Xantonozar@users.noreply.github.com>
2025-07-05 14:25:42 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -15,11 +16,20 @@ const geistMono = localFont({
 export const metadata = {
   title: "Ozly",
   description: "URL shortner for your links",
+  verification: {
+    google: "i1xPv3xUSFmawFqPGNYjokyGB8SG5A5WWjjUtZ1c2a0",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="i1xPv3xUSFmawFqPGNYjokyGB8SG5A5WWjjUtZ1c2a0"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
